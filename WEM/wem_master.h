eb07c507354ac7d8acb949b2c26b14e7a9d1dfbc
L3adCrab <l3adcrab@gmail.com>
2025-09-01 20:54:49 +0300

    feat: master header file

---
+++ WEM/wem_master.h
@@ -0,0 +1,28 @@
+#ifndef wem_master_h
+#define wem_master_h
+
+/*///////////////////////////////////////////////////////////////////////////////////
+//  USAGE
+//  By default functions are defined as extern.
+//  To implement somewhere in source file before including header file
+//  #define WEM_IMPLEMENTATION
+//  Implementation should be defined only once.
+//  
+//  For use as static functions before including header file
+//  #define WEM_STATIC
+//  There if no need to define WEM_IMPLEMENTATION when using as static,
+//  although WEM_STATIC will need to be somewhere defined in every source file where
+//  library will be used. To circumvent this and whole library will be used as static
+//  add the WEM_STATIC define to compiler (gcc/clang -DWEM_STATIC)
+//
+//  To include all weu library in souce file at once, include weu_master.h  
+*////////////////////////////////////////////////////////////////////////////////////
+
+#include "bezier.h"
+#include "mat4.h"
+#include "random.h"
+#include "vec2.h"
+#include "vec3.h"
+#include "vec4.h"
+
+#endif
